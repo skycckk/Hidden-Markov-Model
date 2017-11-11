3726a0d8eb747c43b459f05919f452a98c18cdb6
Wayne Huang <csie.chung@gmail.com>
2017-11-11 09:03:36 -0800

    prepare to implement EM-algorithm. added E-step w/o iteration and a simple test case.

---
+++ EM/my_em.py
@@ -0,0 +1,57 @@
+import numpy as np
+from math import factorial
+
+__author__ = "Wei-Chung Huang"
+__copyright__ = "Copyright 2017"
+__version__ = "1.0.0"
+
+
+def prob_func(x, theta):
+    """
+    Binomial Distribution
+    :param x: int
+            The outcome value
+    :param theta: float
+            The probability at desired Head or Tail
+    :return: float
+            The probability of binomial
+    """
+    n, k = 10, x
+    a = factorial(n)
+    b = factorial(k)
+    c = factorial(n - k)
+    term1 = a / b * c
+    term2 = theta ** k
+    term3 = (1.0 - theta) ** (n - k)
+    return term1 * term2 * term3
+
+
+def e_step(data, theta, tao, n_clusters, n_samples):
+    p = np.zeros([n_clusters, n_samples])
+
+    for i in range(n_samples):
+        bayes_sum = 0
+        x = data[i]
+        for j in range(n_clusters):
+            bayes_sum += tao[j] * prob_func(x, theta[j])
+
+        for j in range(n_clusters):
+            p[j][i] = tao[j] * prob_func(x, theta[j]) / bayes_sum
+
+    # dump each probability p
+    for i in range(n_samples):
+        for j in range(n_clusters):
+            print(p[j][i])
+
+
+def test():
+    n_clusters = 2
+    n_samples = 5
+    x = np.asarray([8, 5, 9, 4, 7])
+    theta = np.asarray([0.6, 0.5])
+    tao = np.asarray([0.7, 0.3])
+
+    e_step(x, theta, tao, n_clusters, n_samples)
+
+if __name__ == '__main__':
+    test()
